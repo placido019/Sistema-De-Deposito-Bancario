42b5ef4d156d93346070e0105da079cca38e55b5
Joao <joaopedroplacidodesouza@gmail.com>
2024-07-14 00:16:38 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema De Deposito Bancario/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaParaDepósitoBancario
+{
+    internal class ContaBancaria
+    {
+        //get = acessado
+        //private = não pode ser alterado
+
+
+        //Atributos
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        //Construtor com 2 argumentos
+        public ContaBancaria(int numeroConta, string titular)
+        {
+            //Atributos recebendo argumentos
+            NumeroConta = numeroConta;
+            Titular = titular;
+            Saldo = 0;
+        }
+
+        //Construtor
+        //"This" reutiliza os parametros do construtor anterior (numero conta e titular)
+        public ContaBancaria(int numeroConta, string titular, double saldo) : this(numeroConta, titular)
+        {
+            //Atributos recebendo argumentos
+            Saldo = saldo;
+
+        }
+
+        //ToString serve como uma "Função" para mostrar a saída dos dados
+        public override string ToString()
+        {
+            return $"Conta: {NumeroConta}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ Sistema De Deposito Bancario/Program.cs
@@ -0,0 +1,48 @@
+﻿using SistemaParaDepósitoBancario;
+using System;
+using System.Globalization;
+
+namespace BancCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando a conta (outra classe)
+            //declarei o construtor para que ele possa ser acessado
+            //em qualquer escopo do meu projeto
+            ContaBancaria conta;
+
+            Console.WriteLine("---- Sistema de depósito bancario (Aperte enter)----");
+            Console.ReadLine();
+
+            Console.Write("Entre o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o títular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.WriteLine("Havera depósito inicial? (S/N)");
+            char resp = char.Parse(Console.ReadLine().ToUpper());
+
+            if (resp == 'S')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double depositoinicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                //Instanciando a conta com os argumentos recebidos nos console.readline
+                conta = new ContaBancaria(numero, titular, depositoinicial);
+
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("---- Obrigado por usar o sistema bancário 1.0 ----");
+        }
+    }
+}
